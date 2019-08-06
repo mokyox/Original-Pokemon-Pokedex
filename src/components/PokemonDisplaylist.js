514c55cbac139cd9e96aee2ec5b9afce7c7d8851
Mo <mohamedjama53@gmail.com>
2019-08-06 16:07:12 +0100

    add searchbox

--- src/components/PokemonDisplaylist.js
+++ src/components/PokemonDisplaylist.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import PokemonCard from "./PokemonCard/PokemonCard";
+import SearchBox from "./searchbox";
 
 class PokemonDisplaylist extends Component {
   constructor() {
@@ -21,26 +22,24 @@ class PokemonDisplaylist extends Component {
   }
   onSearchChange = event => {
     //update state of searchbardata so we can see our pokemon
+    //Arrow functions make sure that the this value is bound to where it was created
     this.setState({ searchbardata: event.target.value });
   };
   render() {
-    //Feature idea: Searchbox that allows you to search for individual Pokemon by name or type
-    // const pokemonFiltered = this.state.pokemon.filter(pokemon => {
-    //   return pokemon.name
-    //     .toLowerCase()
-    //     .includes(this.state.searchbardata.toLowerCase());
-    // });
+    const pokemonFiltered = this.state.pokemon.filter(pokemon => {
+      return pokemon.name
+        .toLowerCase()
+        .includes(this.state.searchbardata.toLowerCase());
+    });
     if (this.state.pokemon.length === 0) {
       return <h1>Loading...</h1>;
     }
     return (
       <div>
         <h1>Pokedex</h1>
-        <div className="row">
-          {/*Feature: searchbox */}
-          {/* <SearchBox searchChange={this.onSearchChange} /> */}
-          {/*Add pokemon={pokemonFiltered back into PokemonList}*/}
-          {this.state.pokemon.map(pokemon => (
+        {<SearchBox searchChange={this.onSearchChange} />}
+        <div className="row ">
+          {pokemonFiltered.map(pokemon => (
             <PokemonCard
               key={pokemon.name}
               name={pokemon.name}
--- src/components/searchbox.js
+++ src/components/searchbox.js
@@ -1,13 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
 const SearchBox = ({ searchChange }) => {
   return (
-    <div className="text-center">
-      <input
-        type="search"
-        placeholder="Search Pokemon"
-        onChange={searchChange}
-      />
+    <div className="text-center p-2 m-2">
+      <form>
+        <div className="form-group col-sm-4 col-xs-1 col-lg-5 mx-auto">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Search Pokemon"
+            onChange={searchChange}
+          />
+        </div>
+      </form>
     </div>
   );
 };
