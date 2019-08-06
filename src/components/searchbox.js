4c4898766541189426f5f32b0fda125269b8ad28
Mo <mohamedjama53@gmail.com>
2019-08-06 17:02:54 +0100

    improve searchbox display

--- src/components/searchbox.js
+++ src/components/searchbox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className="text-center p-2 m-2">
       <form>
-        <div className="form-group col-sm-4 col-xs-1 col-lg-5 mx-auto">
+        <div className="form-group col-lg-4 col-sm-5 col-xs-2  mx-auto">
           <input
             className="form-control"
             type="search"
