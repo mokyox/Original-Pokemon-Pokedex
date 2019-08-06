6b21bb721b53b04d6dbe7cc7a40564e3617d647c
Mo <mohamedjama53@gmail.com>
2019-08-06 16:11:35 +0100

    add container style for lg screen

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -135,7 +135,7 @@ class PokemonCard extends Component {
     const upperCaseFirstChar = string =>
       string.charAt(0).toUpperCase() + string.substring(1);
     return (
-      <div className="card-container col-md-3 col-sm-6 mb-3">
+      <div className="card-container col-lg-3 col-md-4 col-sm-6 mb-3">
         <div className="card bg-dark text-white">
           <div className="text-nowrap p-1">
             <h4 className="pokemon-name">{upperCaseFirstChar(name)}</h4>
