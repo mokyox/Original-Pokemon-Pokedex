4784fc6e8deb929234f576af3ddd10d5b35c669b
Mo <mohamedjama53@gmail.com>
2019-08-12 12:21:02 +0100

    add margin top container for navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import PokemonContainer from "./components/PokemonContainer";
+import Navbar from "./components/navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <div className="container">
           <PokemonContainer />
         </div>
