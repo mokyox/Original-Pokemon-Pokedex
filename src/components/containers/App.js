58dada0b6b5beffd590bc8419e090ecf1c8cdc6f
Mo <mohamedjama53@gmail.com>
2019-08-16 17:22:52 +0100

    change app from class to functional component

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,22 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import PokemonContainer from "../PokemonContainer";
 
-class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <PokemonContainer />
-        </div>
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <PokemonContainer />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
