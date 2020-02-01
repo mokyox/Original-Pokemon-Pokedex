a81192a446ed255578a43655729286b825b5793e
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-02-01 19:26:42 +0000

    refactor logic to use Hooks

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import PokemonCard from "./PokemonCard/PokemonCard";
 import pokedex from "./pokedex";
-import description from "./description";
+import descriptions from "./descriptions";
 import abilities from "./abilities";
 import { setSearchField } from "./actions/actions";
 import Navbar from "./navbar";
@@ -18,67 +18,58 @@ const mapDispatchToProps = dispatch => {
     onSearchChange: event => dispatch(setSearchField(event.target.value))
   };
 };
-class PokemonList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      pokemon: [],
-      description: [],
-      ability: []
-    };
-  }
 
-  async componentDidMount() {
-    const pokemonData = pokedex;
-    const pokemonDescription = description;
-    const pokemonAbilities = abilities.map(ability => {
-      return ability.map(ability => ability.ability.name).join(" / ");
-    });
-    this.setState({
-      pokemon: pokemonData,
-      description: pokemonDescription,
-      ability: pokemonAbilities
-    });
-  }
+const PokemonList = ({ searchField, onSearchChange }) => {
+  const [pokemon, setPokemon] = useState([]);
+  const [description, setDescription] = useState([]);
+  const [ability, setAbility] = useState([]);
 
-  render() {
-    const { description, ability } = this.state;
-    const { searchField, onSearchChange } = this.props;
+  useEffect(() => {
+    //Get initial Pokemon data
+    async function getPokemonData() {
+      const pokemonData = pokedex;
+      const pokemonDescription = descriptions;
+      const pokemonAbilities = abilities.map(ability => {
+        return ability.map(ability => ability.ability.name).join(" / ");
+      });
+      setPokemon(pokemonData);
+      setDescription(pokemonDescription);
+      setAbility(pokemonAbilities);
+    }
+    getPokemonData();
+  }, []);
 
-    const pokemonFiltered = this.state.pokemon.filter(pokemon => {
-      return pokemon.name.english
-        .toLowerCase()
-        .includes(searchField.toLowerCase());
-    });
-
-    return (
-      <div className='row'>
-        <div className='col'>
-          <div>
-            <div className='row'>
-              <Navbar searchChange={onSearchChange} />
-              {pokemonFiltered.map(pokemon => (
-                <PokemonCard
-                  key={pokemon.id}
-                  id={pokemon.id}
-                  name={pokemon.name.english}
-                  image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png?raw=true`}
-                  hp={pokemon.base["HP"]}
-                  attack={pokemon.base["Attack"]}
-                  defense={pokemon.base["Defense"]}
-                  specialAttack={pokemon.base["Sp. Attack"]}
-                  specialDefense={pokemon.base["Sp. Defense"]}
-                  speed={pokemon.base["Speed"]}
-                  type={pokemon.type}
-                  description={description[pokemon.id - 1]}
-                  ability={ability[pokemon.id - 1]}
-                />
-              ))}
-            </div>
-          </div>
+  const pokemonFiltered = pokemon.filter(pokemon => {
+    return pokemon.name.english
+      .toLowerCase()
+      .includes(searchField.toLowerCase());
+  });
+  return (
+    <div className='row'>
+      <div className='col'>
+        <div className='row'>
+          <Navbar searchChange={onSearchChange} />
+          {pokemonFiltered.map(pokemon => (
+            <PokemonCard
+              key={pokemon.id}
+              id={pokemon.id}
+              name={pokemon.name.english}
+              image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png?raw=true`}
+              hp={pokemon.base["HP"]}
+              attack={pokemon.base["Attack"]}
+              defense={pokemon.base["Defense"]}
+              specialAttack={pokemon.base["Sp. Attack"]}
+              specialDefense={pokemon.base["Sp. Defense"]}
+              speed={pokemon.base["Speed"]}
+              type={pokemon.type}
+              description={description[pokemon.id - 1]}
+              ability={ability[pokemon.id - 1]}
+            />
+          ))}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default connect(mapStateToProps, mapDispatchToProps)(PokemonList);
