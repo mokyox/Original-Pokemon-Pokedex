33b4262fa0c717e949d79f229ae90dd5ca2ce08f
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-04-20 19:09:52 +0100

    remove colors list to utils.js

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { PokemonCardStyle } from "./styles";
+import { colors } from "../utils";
 
 const PokemonCard = ({
   name,
@@ -15,27 +16,6 @@ const PokemonCard = ({
   description,
   ability,
 }) => {
-  //https://bulbapedia.bulbagarden.net/wiki/Type
-  const colors = {
-    Normal: "#A8A878",
-    Fighting: "#C03028",
-    Flying: "#A890F0",
-    Poison: "#A040A0",
-    Ground: "#E0C068",
-    Rock: "#B8A038",
-    Bug: "#A8B820",
-    Ghost: "#705898",
-    Steel: "#B8B8D0",
-    Fire: "#F08030",
-    Water: "#6890F0",
-    Grass: "#78C850",
-    Electric: "#F8D030",
-    Psychic: "#F85888",
-    Ice: "#98D8D8",
-    Dragon: "#7538FB",
-    Dark: "#705848",
-    Fairy: "#EE99AC",
-  };
   return (
     <>
       <PokemonCardStyle />
