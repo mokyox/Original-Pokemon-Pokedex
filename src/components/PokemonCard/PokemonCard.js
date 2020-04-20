796d3e1f5de403b8e47c506fc9820c86bdcaf04b
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-04-20 19:03:09 +0100

    remove final scss file and replace with styled-components

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./PokemonCard.css";
+import { PokemonCardStyle } from "./styles";
 
 const PokemonCard = ({
   name,
@@ -13,7 +13,7 @@ const PokemonCard = ({
   speed,
   type,
   description,
-  ability
+  ability,
 }) => {
   //https://bulbapedia.bulbagarden.net/wiki/Type
   const colors = {
@@ -34,57 +34,60 @@ const PokemonCard = ({
     Ice: "#98D8D8",
     Dragon: "#7538FB",
     Dark: "#705848",
-    Fairy: "#EE99AC"
+    Fairy: "#EE99AC",
   };
   return (
-    <div className="card-container col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
-      <div className="card bg-dark text-white">
-        <div className="text-nowrap p-1">
-          <h4 className="pokemon-name">{name}</h4>
-          <p>#{id}</p>
-          {type.map(type => (
-            <span
-              style={{
-                backgroundColor: `${colors[type]}`
-              }}
-              className="p-1 m-1 badge"
-              key={type}
-            >
-              {type}
-            </span>
-          ))}
-        </div>
-        <div className="card-body">
-          <img src={image} alt={`pokemon: ${name}`} />
-          <div className="card-title text-left">
-            <h5 className="pokemon-stats">Stats</h5>
-            <h6 className="pokemon-stat-title">
-              Ability: <span>{ability}</span>
-            </h6>
-            <p className="pokemon-stat-title">
-              HP: <span>{hp}</span>
-            </p>
-            <p className="pokemon-stat-title">
-              Attack: <span>{attack}</span>
-            </p>{" "}
-            <p className="pokemon-stat-title">
-              Defense: <span>{defense}</span>
-            </p>
-            <p className="pokemon-stat-title">
-              Special Attack: <span>{specialAttack}</span>
-            </p>{" "}
-            <p className="pokemon-stat-title">
-              Special Defense: <span>{specialDefense}</span>
-            </p>{" "}
-            <p className="pokemon-stat-title">
-              Speed: <span>{speed}</span>
-            </p>
-            <p className="pokemon-stat-title">Description:</p>
-            <span className="description">{description}</span>
+    <>
+      <PokemonCardStyle />
+      <div className="card-container col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
+        <div className="card bg-dark text-white">
+          <div className="text-nowrap p-1">
+            <h4 className="pokemon-name">{name}</h4>
+            <p>#{id}</p>
+            {type.map((type) => (
+              <span
+                style={{
+                  backgroundColor: `${colors[type]}`,
+                }}
+                className="p-1 m-1 badge"
+                key={type}
+              >
+                {type}
+              </span>
+            ))}
+          </div>
+          <div className="card-body">
+            <img src={image} alt={`pokemon: ${name}`} />
+            <div className="card-title text-left">
+              <h5 className="pokemon-stats">Stats</h5>
+              <h6 className="pokemon-stat-title">
+                Ability: <span>{ability}</span>
+              </h6>
+              <p className="pokemon-stat-title">
+                HP: <span>{hp}</span>
+              </p>
+              <p className="pokemon-stat-title">
+                Attack: <span>{attack}</span>
+              </p>{" "}
+              <p className="pokemon-stat-title">
+                Defense: <span>{defense}</span>
+              </p>
+              <p className="pokemon-stat-title">
+                Special Attack: <span>{specialAttack}</span>
+              </p>{" "}
+              <p className="pokemon-stat-title">
+                Special Defense: <span>{specialDefense}</span>
+              </p>{" "}
+              <p className="pokemon-stat-title">
+                Speed: <span>{speed}</span>
+              </p>
+              <p className="pokemon-stat-title">Description:</p>
+              <span className="description">{description}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/PokemonCard/styles.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+
+const PokemonCardStyle = createGlobalStyle`
+  .img {
+    padding: 30px;
+    margin: 10px;
+    text-align: center !important;
+  }
+
+  .description {
+    font-size: 0.85em;
+  }
+
+  .card-container {
+    display: flex;
+    align-items: stretch;
+  }
+
+  .pokemon-name,
+  .pokemon-stat-title,
+  .pokemon-stats {
+    font-weight: 700;
+    text-transform: capitalize;
+  }
+
+  .pokemon-stats {
+    font-size: 1.5em;
+    margin-bottom: 15px;
+  }
+
+  .pokemon-stat-title {
+    padding: 5px 0px;
+    span {
+      font-weight: 400;
+    }
+  }
+`;
+
+export { PokemonCardStyle };
