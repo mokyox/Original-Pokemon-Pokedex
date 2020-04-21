bcadb2a963229b0dc45774fed07c7d926045a6d0
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-04-21 19:01:52 +0100

    add preventDefault method to stop form input from refreshing page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,33 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 
-//Create a function that filters through list of Pokemon, type, abilities, or whatever
-//When the user inputs a value
-
-//We need a change handler that handles any change dynamically,
-
-// const [searchTerm, setSearchTerm] = useState("");
-
-// const handleChange = (event) => {
-//   setSearchTerm(event.target.value);
-// };
+const handleSubmit = (event) => {
+    event.preventDefault();
+};
 
 const Navbar = ({ searchChange }) => {
-  return (
-    <div>
-      <nav className="navbar navbar-dark bg-dark fixed-top">
-        <h1 className="navbar-brand">Pokedex</h1>
-        <form className="form-inline" onSubmit="">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search Pokemon"
-            aria-label="search"
-            onChange={searchChange}
-          />
-        </form>
-      </nav>
-    </div>
-  );
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-dark fixed-top">
+                <h1 className="navbar-brand">Pokedex</h1>
+                <form className="form-inline" onSubmit={handleSubmit}>
+                    <input
+                        className="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Search Pokemon"
+                        aria-label="search"
+                        onChange={searchChange}
+                    />
+                </form>
+            </nav>
+        </div>
+    );
 };
 
 export default Navbar;
