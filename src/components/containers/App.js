1c64c460afb5ce7988ed20ca5a7a3e5f3251e2f7
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-04-21 13:07:02 +0100

    remove .app class from div

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import PokemonList from "../PokemonList";
 import { GlobalStyle } from "../../styles/styles";
 import "bootstrap/dist/css/bootstrap.css";
+
 const App = () => {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
       <div className="container">
         <PokemonList />
       </div>
-    </div>
+    </>
   );
 };
 
