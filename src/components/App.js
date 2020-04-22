be20ce4915bce53d905507e976ec03e4c22f6b8c
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-04-22 18:12:13 +0100

    update imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import PokemonList from "../PokemonList/PokemonList";
-import { GlobalStyle } from "../../styles/styles";
-import Navbar from "../Navbar";
+import PokemonList from "./PokemonList/PokemonList";
+import { GlobalStyle } from "../styles/styles";
+import Navbar from "./Navbar";
 import "bootstrap/dist/css/bootstrap.css";
 
 const App = () => {
