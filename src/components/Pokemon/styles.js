69059930e5ce5011f17047c5c628cf88bfac3efd
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-05-15 12:56:06 +0100

    remove unused styles

--- src/components/Pokemon/styles.js
+++ src/components/Pokemon/styles.js
@@ -9,14 +9,3 @@ export const Container = styled.div`
         font-size: 1.1rem;
     }
 `;
-
-export const PokemonDescriptionCard = styled.div`
-    margin: 0 auto;
-    padding: 0;
-    flex: 1 1 50%;
-    border: 1px solid #efefef;
-
-    p {
-        margin: 0 0 30px;
-    }
-`;
